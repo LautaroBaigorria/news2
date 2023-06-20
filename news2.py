a52be5614309dd23a06fb1a810d1696a125948e9
Lautaro Baigorria <lautarobaigorria@yahoo.com.ar>
2023-06-20 09:42:14 -0300

    se agrega deteccion de feeds en url

--- news2.py
+++ news2.py
@@ -1,15 +1,17 @@
 #!/usr/bin/python
 import feedparser
-import soupsieve
+# import soupsieve
 import yaml 
 from bs4 import BeautifulSoup
+import os.path
+
 
 class News(object):
     """docstring for News"""
     def __init__(self):
         pass
     def ver_feeds(self):
-         #lista = ['http://rss.slashdot.org/Slashdot/slashdot','http://www.digg.com/rss/index.xml','https://www.tecmint.com/feed/','https://www.reddit.com/r/Python/.rss']
+        #  listaPorDefecto = ['http://rss.slashdot.org/Slashdot/slashdot','http://www.digg.com/rss/index.xml','https://www.tecmint.com/feed/','https://www.reddit.com/r/Python/.rss']
          lista=self.loadyaml()
          self.niltotitle(lista)
          self.print_dictlist(lista)      
@@ -25,7 +27,11 @@ class News(object):
                 print (str(i+1)+ ' - '  + d.entries[i].title)
                 i+=1
             select_item=input('Seleccione articulo: ')
-            article = d.entries[int(select_item)-1].description
+            article = d.entries[int(select_item)-1].title
+            article += '\n'
+            article += d.entries[int(select_item)-1].link
+            article += '\n'
+            article += d.entries[int(select_item)-1].description
             esHtml = bool(BeautifulSoup(article, "html.parser").find())
 
             if (esHtml):
@@ -35,12 +41,25 @@ class News(object):
                 print(article)
 
     def agregarfeed(self):
-        lsita=self.loadyaml()
         nuevo_feed=input('Escriba link de nuevo feed: ')
-        el={"titulo":"","link":nuevo_feed}
-        lsita.append(el)
-        self.feed2yaml(lsita)
-        #self.mostrar_menu()
+        # nuevo_feed=self.checkIfUrlHasHttp(nuevo_feed)
+        nuevo_feed=self.checkIfUrlHasSlash(nuevo_feed)
+        if (self.checkIfUrlHasEntries(nuevo_feed)):
+            lsita=self.loadyaml()
+            el={"titulo":"","link":nuevo_feed}
+            lsita.append(el)
+            self.feed2yaml(lsita)
+            #self.mostrar_menu()
+        else:
+            print ("se ejecuta wrapperCheck")
+            nuevo_feed=self.wrapperCheck(nuevo_feed)
+            if (self.checkIfUrlHasEntries(nuevo_feed)):
+                lsita=self.loadyaml()
+                el={"titulo":"","link":nuevo_feed}
+                lsita.append(el)
+                self.feed2yaml(lsita)
+            else:
+                print ("no se encontro feed valido")    
 
     def borrarfeed(self):
         lista=self.loadyaml()
@@ -56,16 +75,32 @@ class News(object):
 
     def loadyaml(self):
         lsita=[]
-        with open(".newsrc",'r') as my_file:
-            lsita=yaml.load(my_file, Loader=yaml.FullLoader)
+        if os.path.isfile(".newsrc"):
+            with open(".newsrc",'r') as my_file:
+                lsita=yaml.load(my_file, Loader=yaml.FullLoader)
+        else: 
+            lsita = [{"titulo":"","link":'http://rss.slashdot.org/Slashdot/slashdot'},{"titulo":"","link":'http://www.digg.com/rss/index.xml'}] 
         return lsita
         
     def agregarfeedcmdline(self,nuevo_feed):
-        lsita=self.loadyaml()
-        el={"titulo":"","link":nuevo_feed}
-        lsita.append(el)
-        self.feed2yaml(lsita)
-        # self.mostrar_menu()
+        # nuevo_feed=self.checkIfUrlHasHttp(nuevo_feed)
+        nuevo_feed=self.checkIfUrlHasSlash(nuevo_feed)
+        if (self.checkIfUrlHasEntries(nuevo_feed)):
+            lsita=self.loadyaml()
+            el={"titulo":"","link":nuevo_feed}
+            lsita.append(el)
+            self.feed2yaml(lsita)
+            # self.mostrar_menu()
+        else:
+            print ("se ejecuta wrapperCheck")
+            nuevo_feed=self.wrapperCheck(nuevo_feed)
+            if (self.checkIfUrlHasEntries(nuevo_feed)):
+                lsita=self.loadyaml()
+                el={"titulo":"","link":nuevo_feed}
+                lsita.append(el)
+                self.feed2yaml(lsita)
+            else:
+                print ("no se encontro feed valido") 
 
     def print_dictlist(self, array):
         if type(array)==list:
@@ -97,7 +132,7 @@ class News(object):
             dict1["titulo"]=d.feed.title
         return dict1
 
-    def parselink(link):
+    def parselink(self,link):
         d=feedparser.parse(link)
         return d
     
@@ -106,6 +141,7 @@ class News(object):
         text = soup.get_text()
         return text
     
+    '''no funciona'''
     def mostrarNoticiasrecientes(self): 
         lista = self.loadyaml()
         table_titles = []
@@ -119,4 +155,81 @@ class News(object):
         for row in table_data:
             print("{: ^20} {: ^20} {: ^20} {: ^20}".format(*row))    
 
+    def checkIfUrlHasEntries(self,url):
+        posibleFeed = feedparser.parse(url)
+        if not (posibleFeed.entries):
+            print ("posibleFeed.entries esta vacio")
+            return False
+        else: 
+            return True
 
+    def checkIfUrlHasHttp(self,url):
+        if not "http://" or "https://" in url:
+            print("no contiene http")
+            url = "http://{0}".format(url)
+            return url
+        else: 
+            return url
+        
+    def checkIfUrlHasSlash(self,url):
+        if url[-1] != "/":
+            url = "{0}/".format(url)
+        return url
+
+    def checkForWordpressFormat(self,url):
+        url = "{0}feed".format(url)
+        posibleFeed = feedparser.parse(url)
+        if not posibleFeed.entries:
+            print("posibleFeed.entries esta vacio / no es un sitio wordpress")
+        else:
+            print("se encontraron feeds en sitio tipo wordpress")
+            print(url)
+            return url
+
+    def checkForTumblrFormat(self,url):
+        url = "{0}rss".format(url)
+        posibleFeed = feedparser.parse(url)
+        if not posibleFeed.entries:
+            print("posibleFeed.entries esta vacio / no es un sitio tumblr")
+            return False
+        else:
+            print("se encontraron feeds en sitio tipo tumblr")
+            print(url)
+            return True
+        
+    def checkForBloggerFormat(self,url):
+        url = "{0}feeds/posts/default".format(url)
+        posibleFeed = feedparser.parse(url)
+        if not posibleFeed.entries:
+            print("posibleFeed.entries esta vacio / no es un sitio blogger")
+            return False
+        else:
+            print("se encontraron feeds en sitio tipo blogger")
+            print(url)
+            return True
+
+    def checkForMediumFormat(self,url):
+        url = url[:19] + 'feed/' + url[19:]
+        # https://medium.com/feed/example-site
+        posibleFeed = feedparser.parse(url)
+        if not posibleFeed.entries:
+            print("posibleFeed.entries esta vacio / no es un sitio medium")
+            return False
+        else:
+            print("se encontraron feeds en sitio tipo medium")
+            print(url)
+            return True
+        
+    def checkForFeedInSourceCode(self,url):
+        pass
+
+    def wrapperCheck(self,url):
+        if (self.checkForWordpressFormat(url)):
+            url = "{0}feed".format(url)
+        if (self.checkForTumblrFormat(url)):
+            url = "{0}rss".format(url)
+        if (self.checkForBloggerFormat(url)):
+            url = "{0}feeds/posts/default".format(url)
+        if (self.checkForMediumFormat(url)):
+            url = url[:19] + 'feed/' + url[19:]
+        return url
